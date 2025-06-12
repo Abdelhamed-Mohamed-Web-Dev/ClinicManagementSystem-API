9d8ed84482fd1a4d79bd7e26cf1133821f7bac4e
3bdel7amed <a1312004body10@gmail.com>
2025-06-12 07:22:04 +0300

    AddMigration

---
+++ Infrastructure/Presistence/Migrations/20250612042016_AddPaymentIntentToAppointment.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentIntentToAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Appointments",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClintSecret",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentStatus",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "ClintSecret",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentStatus",
+                table: "Appointments");
+        }
+    }
+}
