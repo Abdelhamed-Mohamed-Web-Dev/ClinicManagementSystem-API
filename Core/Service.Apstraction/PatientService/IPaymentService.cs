00eea18a1f7100b06e536c4f32077df827d00c8d
3bdel7amed <a1312004body10@gmail.com>
2025-06-12 06:51:09 +0300

    Payment

---
+++ Core/Service.Apstraction/PatientService/IPaymentService.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Shared.AppointmentModels;
+
+namespace Service.Abstraction.PatientService
+{
+	public interface IPaymentService
+	{
+		public Task<AppointmentDto> CreatePaymentIntentAsync(Guid appointmentId);
+		public Task UpdatePaymentStatusAsync(string request, string header);
+
+	}
+}
