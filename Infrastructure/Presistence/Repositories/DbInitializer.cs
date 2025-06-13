70734958e517c07ed0697f0987db7eeb6e1ad7ff
3bdel7amed <a1312004body10@gmail.com>
2025-06-13 17:38:09 +0300

    marge

--- Infrastructure/Presistence/Repositories/DbInitializer.cs
+++ Infrastructure/Presistence/Repositories/DbInitializer.cs
@@ -129,7 +129,7 @@ namespace Persistence.Repositories
                 await roleManager.CreateAsync(new IdentityRole("Admin"));
             }
 
-            // Seed Defult User
+         //   Seed Defult User
             if (!userManager.Users.Any())
             {
                 var Doctor1 = new User
@@ -139,28 +139,36 @@ namespace Persistence.Repositories
                     UserName = "D01",
                     PhoneNumber = "0102546145",
 
-           //     };
-           //     var Admin1 = new User
-           //     {
-           //         DisplayName = "Mohamed",
-           //         Email = "mohamed@gmail.com",
-           //         UserName = "A01",
-           //         PhoneNumber = "011235165"
+                };
+                var Admin1 = new User
+                {
+                    DisplayName = "Mohamed",
+                    Email = "mohamed@gmail.com",
+                    UserName = "A01",
+                    PhoneNumber = "011235165"
 
-           //     };
+                };
+				var Patient1 = new User
+				{
+					DisplayName = "amr",
+					Email = "amr@gmail.com",
+					UserName = "P01",
+					PhoneNumber = "011235165"
+
+				};
 
 
 
-                await userManager.CreateAsync(Doctor1, "12345678");
+				await userManager.CreateAsync(Doctor1, "12345678");
                 await userManager.CreateAsync(Patient1, "12345678");
                 await userManager.CreateAsync(Admin1, "12345678");
                 await userManager.AddToRoleAsync(Doctor1, "Doctor");
                 await userManager.AddToRoleAsync(Patient1, "Patient");
                 await userManager.AddToRoleAsync(Admin1, "Admin");
 
-                //}
             }
         }
+        }
     }
-}
+
   
